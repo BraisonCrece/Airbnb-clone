dc178888ec56cd554adb664bbe4425c1b9a682fa
Brais Fernandez <braisoncrece@gmail.com>
2022-09-24 12:05:08 +0200

    Seting up log-in/sign-in modal

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,7 +2,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
