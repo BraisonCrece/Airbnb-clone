54f7d88c11b25f1b5233fd51eaeb79fc2b2851f5
Brais Fernandez <braisoncrece@gmail.com>
2022-09-28 12:42:50 +0200

    Model Property

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_23_081208) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_103248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.string "headline"
+    t.text "description"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_081208) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_id"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
---
+++ spec/factories/properties.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :property do
+    name { "MyString" }
+    headline { "MyString" }
+    description { "MyText" }
+    city { "MyString" }
+    state { "MyString" }
+    country { "MyString" }
+  end
+end
