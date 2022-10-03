044cf971d73b2231085ef9e0764da654238a20a6
Brais Fernandez <braisoncrece@gmail.com>
2022-10-03 11:14:03 +0200

    calculate the distance from a property

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,10 +1,21 @@
 import { Controller } from '@hotwired/stimulus'
+import { getDistance, convertDistance } from 'geolib'
 
 export default class extends Controller {
+  static targets = ['property']
   connect() {
     window.navigator.geolocation.getCurrentPosition((position) => {
       this.element.dataset.latitude = position.coords.latitude
       this.element.dataset.longitude = position.coords.longitude
+
+      this.propertyTargets.forEach((propertyTarget) => {
+        let distanceFrom = getDistance(
+          { latitude: position.coords.latitude, longitude: position.coords.longitude },
+          { latitude: propertyTarget.dataset.latitude, longitude: propertyTarget.dataset.longitude }
+        )
+        propertyTarget.querySelector('[data-distance-away]').innerHTML = `${Math.round(convertDistance(distanceFrom,'km'))} kilometers away`
+      });
+
     })
   }
 }
